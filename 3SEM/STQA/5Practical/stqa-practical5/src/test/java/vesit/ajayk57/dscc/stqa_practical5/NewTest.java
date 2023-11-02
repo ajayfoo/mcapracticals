3ac80753f85ab419896980f9a470d8dc3a31c29c
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2023-11-02 09:40:01 +0530

    Use Selenium to demonstrate TestNG annotations

--- 3SEM/STQA/5Practical/stqa-practical5/src/test/java/vesit/ajayk57/dscc/stqa_practical5/NewTest.java
+++ 3SEM/STQA/5Practical/stqa-practical5/src/test/java/vesit/ajayk57/dscc/stqa_practical5/NewTest.java
@@ -1,5 +1,13 @@
 package vesit.ajayk57.dscc.stqa_practical5;
 
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,17 +16,39 @@ public class NewTest {
 	@Test
 	public void f() {
 		System.out.println("f()");
-		assert ("a".equals("a"));
+		assert(true);
+	}
+	@Test
+	public void eightComponents() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+		String title = driver.getTitle();
+		assertEquals("Web form", title);
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+		WebElement textBox = driver.findElement(By.name("my-text"));
+		WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+		textBox.sendKeys("Selenium");
+		submitButton.click();
+
+		WebElement message = driver.findElement(By.id("message"));
+		String value = message.getText();
+		assertEquals("Received!", value);
+
+		driver.quit();
 	}
 
 	@BeforeMethod
 	public void beforeMethod() {
-		System.out.println("Runs before f()");
+		System.out.println("Runs before the test methods");
 	}
 
 	@AfterMethod
 	public void afterMethod() {
-		System.out.println("Runs after f()");
+		System.out.println("Runs after the test methods");
 	}
 
 }
